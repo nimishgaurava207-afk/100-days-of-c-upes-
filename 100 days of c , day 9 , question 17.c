c81aa735c4eb77eead43123a3e708942dd5959d9
nimishgaurava207-afk <nimishgaurava207@gmail.com>
2026-09-18 13:54:12 +0530

    Format code in question 17.c for consistency

--- 100 days of c , day 9 , question 17.c
+++ 100 days of c , day 9 , question 17.c
@@ -3,7 +3,7 @@
 
 int main (){
      float a , b , c , d , r1 , r2;
-    printf(" Enter the values of a, b, c,: ");
+    printf(" Enter the values of a, b, c: ");
     scanf("%f %f %f", &a, &b, &c);
     d = (b * b) - (4 * a * c);
     

798e1c444b648355e0a0bd994fe39e57cc0112ac
nimishgaurava207-afk <nimishgaurava207@gmail.com>
2026-09-18 13:42:32 +0530

    Fix leap year calculation logic in C program
    Updated the leap year program to correctly handle both leap and non-leap years.

--- 100 days of c , day  7 , question 13.c
+++ 100 days of c , day  7 , question 13.c
@@ -2,20 +2,25 @@
 
 int main() {
     int year;
-    printf("Enter number of days in a year: ");
+    printf("Enter the year: ");
     scanf("%d", &year);
     
+    
     if ((year / 100) * 100 == year) {
-
-        if ((year / 400) * 400 == year)
-         {
+        if ((year / 400) * 400 == year) {
             printf("%d is a leap year.\n", year);
-        } else 
-        
-        {
+        } else {
             printf("%d is not a leap year.\n", year);
         }
-        
-        return 0;
     } 
     
+    else {
+        if ((year / 4) * 4 == year) {
+            printf("%d is a leap year.\n", year);
+        } else {
+            printf("%d is not a leap year.\n", year);
+        }
+    }
+    
+    return 0;
+}

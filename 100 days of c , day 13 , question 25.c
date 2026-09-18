2da94cdcf65a15498b26cae965a19bdac4c32b25
nimishgaurava207-afk <nimishgaurava207@gmail.com>
2026-09-18 14:18:45 +0530

    Refactor calculator program with operator input

--- 100 days of c , day 13 , question 25.c
+++ 100 days of c , day 13 , question 25.c
@@ -1,21 +1,50 @@
 #include <stdio.h>
-int main(){
-    int num_1, num_2, sum, difference, product, quotient, remainder;
-    printf("Enter both numbers: ");
-    scanf("%d %d" , &num_1, &num_2);
 
-    sum = num_1 + num_2;
-    difference = num_1 - num_2;
-    product = num_1 * num_2;
-    quotient = num_1 / num_2;
-    remainder = num_1 % num_2;
+int main()
+{
+    int a, b;
+    char op;
 
-    printf("The sum of both numbers is %d\n" , sum);
-    printf("The difference of both numbers is %d\n" , difference);
-    printf("The product of both numbers is %d\n" , product);
-    printf(" The quotient of both numbers is %d\n" , quotient);
-    printf("The remainder of both numbers is %d\n" , remainder);
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter operator (+, -, *, /, %%): ");
+    scanf(" %c", &op);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch (op)
+    {
+        case '+':
+            printf("Result = %d", a + b);
+            break;
+
+        case '-':
+            printf("Result = %d", a - b);
+            break;
+
+        case '*':
+            printf("Result = %d", a * b);
+            break;
+
+        case '/':
+            if (b != 0)
+                printf("Result = %d", a / b);
+            else
+                printf("Cannot divide by zero");
+            break;
+
+        case '%':
+            if (b != 0)
+                printf("Result = %d", a % b);
+            else
+                printf("Cannot divide by zero");
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
 
     return 0;
-
 }
